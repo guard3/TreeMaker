b12fde93c6b6ede8ae423f8602c5a496ee5cbf38
guard3 <cblsss2011@hotmail.com>
2024-07-09 13:41:16 +0300

    Compile tmNLCOTester

--- Source/test/tmNLCOTester/tmNLCOTester.cpp
+++ Source/test/tmNLCOTester/tmNLCOTester.cpp
@@ -3,15 +3,13 @@ File:         tmNLCOTester.cpp
 Project:      TreeMaker 5.x
 Purpose:      Test application for the nonlinear constr. optimization algorithms
 Author:       Robert J. Lang
-Modified by:  
+Modified by:  Konstantinos Bolosis
 Created:      2004-03-05
 Copyright:    ©2004 Robert J. Lang. All Rights Reserved.
 *******************************************************************************/
 
 #include "tmNLCO.h"
 #include "tmNLCOTester.h"
-
-using namespace std;
 
 /**********
 Utility routines for reporting output
@@ -20,34 +18,30 @@ Utility routines for reporting output
 /*****
 Write the value of a vector to the console
 *****/
-void ReportVector(const char* name, const vector<double>& v)
-{
-  cout << name << " = (";
-  for (size_t i = 0; i < v.size(); ++i) {
-    cout << v[i];
-    if (i != v.size() - 1) cout << ", ";
-  }
-  cout << ")\n";
+void ReportVector(std::string_view name, const std::vector<double>& v) {
+	std::cout << name << " = (";
+	if (auto it = v.begin(); it != v.end()) {
+		std::cout << *it;
+		while (++it != v.end())
+			std::cout << ", " << *it;
+	}
+	std::cout << ")\n";
 }
 
 /*****
 Write the value of a vector in which consecutive values make up ordered pairs.
 *****/
-void ReportPairVector(const char* name, const vector<double>& v)
-{
-  size_t n = v.size() / 2;
-  for (size_t i = 0; i < n; ++i)
-    cout << name << "[" << i << "] = " << v[2*i] << ", " << v[2*i+1] << ")\n";
+void ReportPairVector(std::string_view name, const std::vector<double>& v) {
+	for (std::size_t n = v.size() / 2, i = 0; i < n; ++i)
+		std::cout << name << '[' << i << "] = " << v[2*i] << ", " << v[2*i+1] << ")\n";
 }
 
 /*****
 Write the number of function and gradient calls for a single function.
 *****/
-void ReportCalls(const char* name, tmDifferentiableFn* f)
-{
+void ReportCalls(std::string_view name, tmDifferentiableFn* f) {
 #if TM_PROFILE_OPTIMIZERS 
-  cout << name << " calls = (" << f->GetNumFuncCalls() << ", " <<
-    f->GetNumGradCalls() << ")\n";
+	std::cout << name << " calls = (" << f->GetNumFuncCalls() << ", " << f->GetNumGradCalls() << ")\n";
 #endif
 }
 
@@ -55,16 +49,14 @@ void ReportCalls(const char* name, tmDifferentiableFn* f)
 /*****
 Write the total number of function and gradient calls for a list of functions.
 *****/
-void ReportCalls(const char* name, const std::vector<tmDifferentiableFn*>& flist)
-{
+void ReportCalls(std::string_view name, const std::vector<tmDifferentiableFn*>& flist) {
 #if TM_PROFILE_OPTIMIZERS 
-  size_t numFunc = 0;
-  size_t numGrad = 0;
-  for (size_t i = 0; i < flist.size(); ++i) {
-    numFunc += flist[i]->GetNumFuncCalls();
-    numGrad += flist[i]->GetNumGradCalls();
-  }
-  cout << name << " calls = (" << numFunc << ", " << numGrad << ")\n";
+	std::size_t numFunc = 0, numGrad = 0;
+	for (auto f : flist) {
+		numFunc += f->GetNumFuncCalls();
+		numGrad += f->GetNumGradCalls();
+	}
+	std::cout << name << " calls = (" << numFunc << ", " << numGrad << ")\n";
 #endif
 }
   
@@ -72,25 +64,20 @@ void ReportCalls(const char* name, const std::vector<tmDifferentiableFn*>& flist
 /*****
 Write the feasibility for the given lists of equality and inequality constraints
 *****/
-void ReportFeasibility(const vector<double>& x, 
-  const vector<tmDifferentiableFn*>& eq_list,
-  const vector<tmDifferentiableFn*>& ineq_list)
-{
-  // Record worst-case violation of equality constraint for this x
-  double eq_bad = 0.0;
-  for (size_t i = 0; i < eq_list.size(); ++i) {
-    double fval = fabs(eq_list[i]->Func(x));
-    if (eq_bad < fval) eq_bad = fval;
-  }
-  
-  // Record worst-case violation of inequality constraint for this x
-  double ineq_bad = 0.0;
-  for (size_t i = 0; i < ineq_list.size(); ++i) {
-    double fval = ineq_list[i]->Func(x);
-    if (eq_bad < fval) ineq_bad = fval;
-  }  
-  cout << "equality violation = " << eq_bad << 
-    ", inequality violation = " << ineq_bad << endl;
+void ReportFeasibility(const std::vector<double>& x, const std::vector<tmDifferentiableFn*>& eq_list, const std::vector<tmDifferentiableFn*>& ineq_list) {
+	// Record worst-case violation of equality constraint for this x
+	double eq_bad = 0.0;
+	for (auto f : eq_list) {
+		if (double fval = std::fabs(f->Func(x)); eq_bad < fval)
+			eq_bad = fval;
+	}
+	// Record worst-case violation of inequality constraint for this x
+	double ineq_bad = 0.0;
+	for (auto f : ineq_list) {
+		if (double fval = f->Func(x); eq_bad < fval)
+			ineq_bad = fval;
+	}
+	std::cout << "equality violation = " << eq_bad << ", inequality violation = " << ineq_bad << '\n';
 }
 
 
@@ -98,48 +85,34 @@ void ReportFeasibility(const vector<double>& x,
 Main program. Run all of our test routines.
 *****/
 
-int main()
-{
+int main() {
 #ifdef tmUSE_CFSQP
-  cout << "\n\n";
-  cout << "***************************************\n";
-  cout << "Testing class tmNLCO_cfsqp.\n";
-  cout << "***************************************\n";
-  cout << "\n\n";
-
-  Test_all<tmNLCO_cfsqp>();
+	std::cout << "***************************************\n"
+	             "Testing class tmNLCO_cfsqp.\n";
+	             "***************************************\n\n\n";
+	Test_all<tmNLCO_cfsqp>();
 #endif // tmUSE_CFSQP
 
 #ifdef tmUSE_RFSQP
-  cout << "\n\n";
-  cout << "***************************************\n";
-  cout << "Testing class tmNLCO_rfsqp.\n";
-  cout << "***************************************\n";
-  cout << "\n\n";
-  
-  Test_all<tmNLCO_rfsqp>();
+	std::cout << "***************************************\n"
+	             "Testing class tmNLCO_rfsqp.\n"
+	             "***************************************\n\n\n";
+	Test_all<tmNLCO_rfsqp>();
 #endif // tmUSE_RFSQP
   
 #ifdef tmUSE_ALM
-  cout << "\n\n";
-  cout << "***************************************\n";
-  cout << "Testing class tmNLCO_alm.\n";
-  cout << "***************************************\n";
-  cout << "\n\n";
-  
-  Test_all<tmNLCO_alm>();
+	std::cout << "***************************************\n"
+	             "Testing class tmNLCO_alm.\n"
+	             "***************************************\n\n\n";
+	Test_all<tmNLCO_alm>();
 #endif // tmUSE_ALM
   
 #ifdef tmUSE_WNLIB
-  cout << "\n\n";
-  cout << "***************************************\n";
-  cout << "Testing class tmNLCO_wnlib.\n";
-  cout << "***************************************\n";
-  cout << "\n\n";
-  
-  Test_all<tmNLCO_wnlib>();
+	std::cout << "***************************************\n"
+	             "Testing class tmNLCO_wnlib.\n"
+				 "***************************************\n\n\n";
+	Test_all<tmNLCO_wnlib>();
 #endif // tmUSE_WNLIB
-  
   return 0;
 }
 
